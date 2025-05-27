11f7dca0eb685ebfda072e6f21572ee617dfcaf5
stibiums <1360023994@qq.com>
2025-05-27 15:35:27 +0800

    尝试更改数据库的结构以存储更多信息

--- WordLite/back_head/utils.h
+++ WordLite/back_head/utils.h
@@ -7,30 +7,35 @@
 #include <QString>
 #include <QVector>
 #include <QDateTime>
+#include <QMap>
+
+// 音标和发音结构体
+struct Phonetic {
+    QString text;
+    QString audio;
+};
+
+// 定义结构体来存储每个释义的信息
+struct Definition {
+    QString definition;
+    QString example;
+    QVector<QString> synonyms;
+    QVector<QString> antonyms;
+};
 
 class Word // 存储单词的基本信息，包括单词、发音、释义、例句等，同时记录学习相关信息如最后复习时间、复习次数和难度。
 {
 public:
     int id;
     QString word;
-    QString pronunciation;
-    QString meaning;
-    QString example;// 例句
+    QVector<Phonetic> phonetics;
+    QMap<QString, QVector<Definition>> meanings;
     QDateTime lastReviewed=QDateTime::currentDateTime(); // 初始化为添加时的时间
     int reviewCount=0; // 复习次数
     int difficulty=0; // 1-5，1表示最简单，5表示最难（或者是用户的掌握程度）
 
-    Word(QString word,QString meaning="none"):word(word),meaning(meaning){}
+    Word(QString word,QString meaning="none"):word(word){}
     Word(){}
-    // 成员外部接口
-    int g_id()const{return id;}
-    QString g_word()const{return word;}
-    QString g_prounuciation()const{return pronunciation;}
-    QString g_meaning()const{return meaning;}
-    QString g_example()const{return example;}
-    QDateTime g_lastReviewed()const{return lastReviewed;}
-    int g_reviewCount()const{return reviewCount;}
-    int g_difficulty()const{return difficulty;}
 };
 
 QDebug operator<<(QDebug o,const Word& w);
